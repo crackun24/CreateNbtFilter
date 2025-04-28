aaed2a658fd05d7c22ee2df792e3f528b47413b0
crackun24 <1647923341@qq.com>
2025-04-28 14:17:57 +0800

    反馈对象

---
+++ src/main/java/mcsls/xyz/create_nbt_filter/model/FeedBackJson.java
@@ -0,0 +1,14 @@
+package mcsls.xyz.create_nbt_filter.model;
+
+public class FeedBackJson {
+    private String uuid;//唯一标识码
+    private long time;//启动的时间
+    private String modType;//模组的类型
+
+    public FeedBackJson(String uuid,long time,String mod_type)
+    {
+        this.uuid = uuid;
+        this.time = time;
+        this.modType= mod_type;
+    }
+}
